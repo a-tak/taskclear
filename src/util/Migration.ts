c9021bec82a03c0125b1440de4e51acfc5d68fda
A-tak <hoge+git@a-tak.com>
2018-12-01 15:24:36 +0900

    closed #47  DBマイグレーションの際に500件毎でコミットするように修正

--- src/util/Migration.ts
+++ src/util/Migration.ts
@@ -24,30 +24,55 @@ export default class Migration {
      * @param uid ユーザーid
      */
     private static async migrationIsDeletedAdd(uid: string): Promise<void> {
-        const batch: firestore.WriteBatch = firestore().batch();
+        let batch: firestore.WriteBatch = firestore().batch();
 
-        firebase
-        .firestore()
-        .collection('users')
-        .doc(uid)
-        .collection('tasks')
-        .get()
-        .then((querySnapshot): void => {
-            querySnapshot.forEach((doc: firestore.QueryDocumentSnapshot): void => {
+        const querySnapshot: firestore.QuerySnapshot =
+            await firebase
+            .firestore()
+            .collection('users')
+            .doc(uid)
+            .collection('tasks')
+            .get();
+
+        let count = 0;
+        querySnapshot.forEach((async (doc: firestore.QueryDocumentSnapshot): Promise<void> => {
+            const data: firebase.firestore.DocumentData | undefined = doc.data();
+            if (data.isDeleted === undefined) {
+                count++;
+                if (count > 500) {
+                    try {
+                        await batch.commit();
+                    } catch (e) {
+                        // tslint:disable-next-line:no-console
+                        console.error('is Deleted Flag Write Error!', e);
+                    }
+                    batch = firestore().batch();
+                }
                 batch.update(doc.ref, {isDeleted: false});
-            });
-            batch.commit().then((): void => {
-                firebase
+            }
+        }));
+
+        if (count > 0) {
+            try {
+                    await batch.commit();
+                } catch (e) {
+                    // tslint:disable-next-line:no-console
+                    console.error('is Deleted Flag Write Last Error!', e);
+                }
+        }
+        // DBバージョン情報書き込み
+        try {
+            await firebase
                 .firestore()
                 .collection('users')
                 .doc(uid)
                 .collection('version')
                 .doc('task-is-deleted-flag')
                 .set({done: true});
-                });
-        });
-
-
+        } catch (e) {
+            // tslint:disable-next-line:no-console
+            console.error('DB Version Write Error!', e);
+        }
     }
 }
 
