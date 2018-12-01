7620ba6d40ca8896ba9eb00994f3ce4c29166097
A-tak <hoge+git@a-tak.com>
2018-12-01 23:41:30 +0900

    インデントの修正のみ

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,12 @@ import './registerServiceWorker';
 Vue.config.productionTip = false;
 
 const config = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
 
@@ -26,7 +26,7 @@ const settings = {timestampsInSnapshots: true};
 firestore.settings(settings);
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+    router,
+    store,
+    render: (h) => h(App),
 }).$mount('#app');
