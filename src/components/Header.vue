0072d5f30d2b74365c9ec10ab2e2fff325975208
A-tak <hoge+git@a-tak.com>
2018-12-16 18:17:24 +0900

    #64 リファクタリング

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 import firebase from 'firebase';
 
 @Component
