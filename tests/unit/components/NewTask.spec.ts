fd67d75343a181ecdb2dd57ce2a7fee4886b491e
A-tak <hoge+git@a-tak.com>
2019-01-06 10:38:29 +0900

    ストアが含まれているからだろうがテストが通らない

---
+++ tests/unit/components/NewTask.spec.ts
@@ -0,0 +1,66 @@
+import { mount, Wrapper, shallowMount, createLocalVue } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+import NewTask from '@/components/NewTask.vue';
+import TaskController from '@/lib/TaskController';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+Vue.use(Vuetify);
+
+// ストアが含まれているからだろうがテストが通らない
+// → TypeError: Cannot read property 'split' of undefined
+//
+// https://tech.fusic.co.jp/web/vue-component-testing-with-vuex/
+describe('NewTask.vue', () => {
+  // let wrapper: Wrapper<NewTask>;
+  // let addStub: () => {};
+  // let store;
+  // let tsMutations;
+
+  // beforeEach(() => {
+  //   tsMutations = {
+  //     addTask: jest.fn(),
+  //   };
+  //   store = new Vuex.Store({
+  //     modules: {
+  //       taskList: {
+  //         state: {
+  //           targetDate: new Date(),
+  //           user: {
+  //             uid: 'test',
+  //           },
+  //           taskCtrl: new TaskController(),
+  //         },
+  //         mutations: tsMutations,
+  //       },
+  //     },
+  //   });
+  //   wrapper = mount(NewTask, {
+  //     mocks: {
+  //       $vuetify: { breakpoint: {} },
+  //     },
+  //     store,
+  //     localVue,
+  //   });
+  //   addStub = jest.fn();
+  // });
+
+  it('登録ボタン押せているか確認', () => {
+    // // メソッドをスタブに置き換え
+    // wrapper.setMethods( { addTask: addStub});
+    // // ボタンを押す
+    // const addBtn = wrapper.find('#newtask-add');
+    // addBtn.trigger('click');
+    // // 判定
+    // expect(addStub).toHaveBeenCalled();
+  });
+
+  it('登録イベント発生確認', () => {
+    // //    wrapper.vm.$emit('addedEvent');
+    //     const btn = wrapper.find('#newtask-add');
+    //     btn.trigger('click');
+    //     expect(wrapper.emitted().addedEvent).toBeTruthy();
+  });
+});
