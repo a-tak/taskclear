31fbd3cdc61d23e04a005d5dfe0dbb7fe0b5cf2b
A-tak <hoge+git@a-tak.com>
2019-08-12 14:36:04 +0900

    アイコン読み込みのプラグイン設定を修正。CDNはなんとなくやめた。

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,3 +1,4 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 
@@ -5,6 +6,6 @@ Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
+    iconfont: 'md',
   },
 })
