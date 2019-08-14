fa9f0a62197dcf0b43cd1f6544bb983498eb5c95
A-tak <hoge+git@a-tak.com>
2019-08-14 12:22:32 +0900

    ルーターのhistoryモードの再実装。リロードしても404にならないように修正。

--- src/router.ts
+++ src/router.ts
@@ -7,7 +7,7 @@ import SectionList from '@/views/SectionList.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     { path: '*', redirect: 'login' },
