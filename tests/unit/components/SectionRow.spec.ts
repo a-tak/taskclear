5359fe4c9f4c9a73f37394ada530a74c6a533d50
A-tak <hoge+git@a-tak.com>
2020-09-13 07:44:01 +0000

    テストコード変更試行

--- tests/unit/components/SectionRow.spec.ts
+++ tests/unit/components/SectionRow.spec.ts
@@ -1,34 +1,51 @@
-import { mount, Wrapper, shallowMount } from "@vue/test-utils"
-import SectionRow from "@/components/SectionRow.vue"
 import Vue from "vue"
 import Vuetify from "vuetify"
+
+import SectionRow from "@/components/SectionRow.vue"
 import Section from "@/lib/Section"
 
+import { mount, createLocalVue } from "@vue/test-utils"
+
 Vue.use(Vuetify)
+const localVue = createLocalVue()
 
 describe("SectionRow.vue", () => {
+  let vuetify: typeof Vuetify
+
   const time: string = "8:00"
   const section = new Section()
   section.title = "テストセクション"
   section.startTime = new Date("2018-01-02 " + time + ":00")
-  let wrapper: Wrapper<SectionRow>
 
   beforeEach(() => {
-    wrapper = mount(SectionRow, {
-      mocks: {
-        $vuetify: { breakpoint: {} },
-      },
-      propsData: {
-        section_: section,
-      },
+    vuetify = new Vuetify({
+      breakpoint: {
+        mobileBreakpoint: "md"
+      }
     })
+    // wrapper = mount(SectionRow, {
+    //   mocks: {
+    //     $vuetify: { breakpoint: {} },
+    //   },
+    //   propsData: {
+    //     section_: section,
+    //   },
+    // })
   })
 
   it("propsが渡るか?", () => {
+    const wrapper = mount(SectionRow, {
+      localVue,
+      vuetify,
+    })
     expect(wrapper.props().section_.title).toBe(section.title)
   })
 
   it("タイトルテキストフィールドがあるか?", () => {
+    const wrapper = mount(SectionRow, {
+      localVue,
+      vuetify,
+    })
     const titleField = wrapper.find("#section-title-field-" + section.id)
     expect(titleField.element.tagName).toBe("INPUT")
   })
@@ -38,6 +55,4 @@ describe("SectionRow.vue", () => {
   //   const titleField = wrapper.find('#section-title-field-' + section.id);
   //   expect(titleField.html()).toMatch(section.title);
   // });
-
 })
-
